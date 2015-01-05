d30a3731ff5e3da576027d5e295195d7313ad16a
MatthewAtallah <mathieu.atallah@gmail.com>
2015-01-05 21:11:17 +0200

    Commit4

--- Ift215MatthewAtallah/src/carSeller/CarSeller2.java
+++ Ift215MatthewAtallah/src/carSeller/CarSeller2.java
@@ -46,5 +46,55 @@ public class CarSeller2 {  private int productionyear;
     public void setproductionyear(int productionyear){
         this.productionyear=productionyear;
     }
+        public int getPrice(){
+        int fuelp, dieselp, electricalp, acPrice,windowsp, price;
+        switch(productionyear){
+            case"2012":
+                fuelp=10000;
+                dieselp=9500;
+                electricalp=11500
+                ;
+                break;
+            case "2013":
+                fuelp=12000;
+                dieselp=11500;
+                electricalp=13500
+                ;
+                break;
+             case "2014":
+                fuelp=14000;
+                dieselp=13500;
+                electricalp=15500
+                ;
+                break;
+             case "2015":
+                fuelp=16000;
+                dieselp=15500;
+                electricalp=17500
+                ;
+                break;     
+            default: 
+               fuelp=0;
+               dieselp=0;
+               electricalp=0;
+               acPrice=0;
+        }
+        switch(windows){
+             case "Electricale":
+                 windowsp=500;
+                 break;
+             case "Manual":
+                 windowsp=0;
+                 break;
+                 
+        }
+        {if(ac==false)
+            acPrice=0;
+            else acPrice=500;
+        }
+        price =fuelp+dieselp+electricalp+acPrice+windowsp; 
+        return price;
+        
+    }
     
 }
